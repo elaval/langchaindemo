ccd71ce68eaae6a205cb71f44aa6d728faf87444
Ernesto Laval <ernesto.laval@gmail.com>
2023-04-10 19:18:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,14 @@ st.set_page_config(page_title="Analize growth", page_icon=":robot:")
 openai_api_key = get_api_key()
 st.header("Analize growth")
 
-st.image(image='data.png', width=500)
 
 
 
  
 st.markdown("Analize data for growth (GDP per capita) for countries in the world [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and based on the work by \
                 [@GregKamradt](https://twitter.com/GregKamradt). \n\n View ORIGINAL Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+
+st.image(image='data.png', width=500)
 
 st.markdown("## Enter Your Question")
 
@@ -46,12 +47,13 @@ def update_text_with_example():
 
 st.markdown("### Your Converted Email:")
 
+agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key), 'gdp-per-capita-growth.csv', verbose=True)
+ 
 if question:
     if not openai_api_key:
         st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
         st.stop()
 
-    agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key), 'gdp-per-capita-growth.csv', verbose=True)
     answer = agent.run(question)
     st.write(answer)
 
